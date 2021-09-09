4337901b0da98f4f44825ee4dcfa597d92eeae75
Nikunj66 <17bmiit108@gmail.com>
2021-09-09 23:42:43 +0530

    Controller change

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -11,7 +11,18 @@ export async function createUser(req: Request, res: Response){
 	});
 
 	await user.save().then(()=>{
-		res.json(user);
+		res.status(200).send({
+		"success": true,
+        "statusCode": 200,
+        "message": "User registered successfully",
+        "result": user
+		});
+	}).catch((err)=>{
+		res.status(200).send({
+		"success": false,
+        "statusCode": 200,
+        "message": "Something went wrong!!! Try again.",
+		});
 	});
 
 	
@@ -19,18 +30,92 @@ export async function createUser(req: Request, res: Response){
 
 
 export async function getAll(req:Request,res:Response){
-	res.json(await User.find());
+	const user=await User.find();
+	if(!user)
+	{
+		res.status(200).send({
+		"success": false,
+        "statusCode": 200,
+        "message": "Something went wrong!!! Try again",
+        "result":user 
+		});
+	}
+	else
+	{
+		res.status(200).send({
+		"success": true,
+        "statusCode": 200,
+        "message": "Successfully get the data of User",
+        "result":user 
+		});
+	}
 }
 
 export async function getOne(req: Request, res:Response){
-	res.json(await User.findOne(req.params.id))
+	const user=await User.findOne(req.params.id);
+	if(!user)
+	{
+		res.status(200).send({
+		"success": false,
+        "statusCode": 200,
+        "message": "Something went wrong!!! Try again",
+        "result":user 
+		});
+	}
+	else
+	{
+		res.status(200).send({
+		"success": true,
+        "statusCode": 200,
+        "message": "Successfully get the data of Perticular User",
+        "result":user 
+		});
+	}
 }
 
 export async function deleteUser(req:Request,res:Response){
-	res.json(await User.delete(req.params.id))
+	const user=await User.delete(req.params.id);
+	if(!user.affected)
+	{
+		res.status(200).send({
+		"success": false,
+        "statusCode": 200,
+        "message": "Something went wrong!!! Try again",
+        "result":user 
+		});
+		
+	}
+	else
+	{
+		res.status(200).send({
+		"success": true,
+        "statusCode": 200,
+        "message": "Successfully Deleted User.",
+        "result":user 
+		});
+		
+	}
 }
 
 export async function updateUser(req:Request,res:Response){
 	const uid:number=+req.params.id;
-	res.json(await User.update({ id:  uid},req.body));
+	const user=await User.update({ id:  uid},req.body);
+	if(!user.affected)
+	{
+		res.status(200).send({
+		"success": false,
+        "statusCode": 200,
+        "message": "Something went wrong!!! Try again",
+        "result":user 
+		});
+	}
+	else
+	{
+		res.status(200).send({
+		"success": true,
+        "statusCode": 200,
+        "message": "Successfully updated User.",
+        "result":user 
+		});
+	}
 }
